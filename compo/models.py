a435730256fb4de736a68aac14837fc5ef8ce2dc
jk <jk@redpai.no>
2012-10-22 18:09:01 +0200

    Trader and Company models

--- compo/models.py
+++ compo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Trader(models.Model):
+    email = models.EmailField()
+    company1 = models.ForeignKey(Company)
+    company2 = models.ForeignKey(Company)
+    company3 = models.ForeignKey(Company)
+
+
+class Company(models.Model):
+    name = models.CharField()
+    ticker = models.CharField(max_length=10)
+    start_price = models.DecimalField()
+    end_price = models.DecimalField()
+    #current_price = models.DecimalField()
