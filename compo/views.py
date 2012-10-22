b5b09d32ca8d590debd4808b48ba69552bd75433
jk <jk@redpai.no>
2012-10-22 18:23:23 +0200

    Template

--- compo/views.py
+++ compo/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from compo.models import Trader, Company, TraderForm
+from django.template import RequestContext
+from django.template.loader import get_template
+from django.http import HttpResponse
+
+def join_compo_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = TraderForm()
+        t = get_template('compo.html')
