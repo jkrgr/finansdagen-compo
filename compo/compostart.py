5bf494dc956bea262d30bf8f84b0b9a582c21327
lapstue <lapstue@gmail.com>
2012-10-23 13:51:20 +0200

    Removed stocklist header, and added some debug prints

--- compo/compostart.py
+++ compo/compostart.py
@@ -1,5 +1,8 @@
 from tickerparse import tickerparse
 from stockgetter import netfonds_price
-from models import *
+#from models import *
+start = time.time()
 for pair in tickerparse():
-	print pair
+	print pair[1]
+	ticker = pair[1]+".OSE"
+	print netfonds_price(ticker)
